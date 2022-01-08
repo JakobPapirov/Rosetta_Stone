45cd9c30859060b99cef9630af5d5993bcc05725
Jakob Papirov <JakobPapirov@users.noreply.github.com>
2022-01-08 02:36:37 +0100

    Rust!
    A few hours later?! Finally!
    
    I encountered a fairly common windows10 problem with link.exe. Solution was https://gitanswer.com/rustup-link-exe-problem-on-windows-10-rust-338009649 which lead me to https://github.com/rust-lang/rust/issues/43039
    
    Apparently, this can occur when you decide to download specific components yourself instead of downloading the entire Visual Studio (can't find the exact reference).
    
    ```error: linker `link.exe` not found
      |
      = note: The system cannot find the file specified. (os error 2)
    
    note: the msvc targets depend on the msvc linker but `link.exe` was not found
    
    note: please ensure that VS 2013, VS 2015, VS 2017 or VS 2019 was installed with the Visual C++ option
    
    error: aborting due to previous error```
    
    * In Visual Studio I needed to download in `Desktop development with C++` MSBuild Tools
    * Core: C++ Build Tools core feature
    * C++ 2019 Redistributable Update
    * C++ core desktop feature
    
    * Optional: MSVC v142 - VS 2019 C++ x64/x86 build tools (latest) <- was the last one I didn't have
    * Windows 10 SDK (10.0.16299.0)
    
    Interestingly enough there was never an issue with the missing ```userenv.lib ``` as in the GH issue linked.
    
    I believe that's it.

---
+++ Rust/HelloWorld.rs
@@ -0,0 +1,8 @@
+// https://www.rust-lang.org/tools/install
+// https://doc.rust-lang.org/rust-by-example/hello.html
+// https://docs.microsoft.com/en-us/windows/dev-environment/rust/setup
+
+fn main() {
+
+	println!("Hello World!");
+}
