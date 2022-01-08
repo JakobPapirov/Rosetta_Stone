e7801fbdf84a967d8e516c82d6f61758d28b6f1c
Jakob Papirov <JakobPapirov@users.noreply.github.com>
2022-01-08 01:24:31 +0100

    Free Pascal
    Hello World of Free Pascal

---
+++ Pascal/helloWorld.pas
@@ -0,0 +1,11 @@
+// https://www.osradar.com/how-to-install-free-pascal-on-windows-10/
+// https://www.freepascal.org/docs-html/ref/refse2.html
+// https://sourceforge.net/projects/freepascal/files/Win32/3.2.2/
+// https://wiki.freepascal.org/Hello,_World
+// https://www.tutorialspoint.com/pascal/pascal_program_structure.htm
+
+program helloWorld;
+begin
+	writeln('Hello World!');
+	// not writeIn or writeLn
+end.
