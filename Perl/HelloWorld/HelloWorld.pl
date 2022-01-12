2885f3e6b9640e17c9fbe6a16c4554a188ae2abe
Jakob Papirov <JakobPapirov@users.noreply.github.com>
2022-01-12 20:29:56 +0100

    Added Perl "Hello World!"

---
+++ Perl/HelloWorld/HelloWorld.pl
@@ -0,0 +1,14 @@
+#!K:\Strawberry\perl\bin
+# Messed up the install; Perl installed in the same directory as to where I had the package downloaded
+
+use strict;
+use warnings;
+
+# https://learn.perl.org/installing/windows.html
+# https://code.google.com/archive/p/padre-perl-ide/downloads
+# https://learn.perl.org/first_steps/
+# https://www.youtube.com/watch?v=0cupB4XOU84
+# https://www.youtube.com/watch?v=R05XpUH876I
+# https://padre.perlide.org/
+
+print("Hello World!");
